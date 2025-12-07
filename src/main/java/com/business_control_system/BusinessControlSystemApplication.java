7a2bfb7df25a7e2c1e3e080ed88f3ec82590149f
Raul Damian Rafael <raul.damian220@gmail.com>
2025-12-06 22:45:46 -0600

    primer commit

---
+++ src/main/java/com/business_control_system/BusinessControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.business_control_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business_control_system/BusinessControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business_control_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
